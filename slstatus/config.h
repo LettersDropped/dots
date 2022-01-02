784d03ed4d22cd57b8bfb3ac850d34af46daa914
LettersDropped <91020901+LettersDropped@users.noreply.github.com>
2022-01-02 20:26:40 +0530

    Add files via upload

--- slstatus/config.h
+++ slstatus/config.h
@@ -65,8 +65,10 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ ram_perc, "[ RAM  %s%% ] ", NULL },
-	{ cpu_perc, "[ CPU  %s%% ] ", NULL },
+	{ run_command, "[  %s ] ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
+	{ ram_perc, "[  %s%% ] ", NULL },
+	//{ cpu_perc, "[ CPU  %s%% ] ", NULL },
 	{ battery_perc, "[  %s%% ] ", "BAT0" },
-	{ datetime, "[  %s ]",           "%F %T" },
+	{ datetime, "[  %s ]",	  "%F" },
+	{ datetime, "[  %s ]",           "%I:%M" },
 };
